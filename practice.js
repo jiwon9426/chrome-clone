37218f74173cb8b2f7c3a54c71e2d856becae182
박지원 <jiwon9426@korea.ac.kr>
2021-10-17 18:08:48 +0900

    2021.10.17

--- practice.js
+++ practice.js
@@ -32,3 +32,24 @@ const player2 = {
   },
 };
 player2.sayHello("jiwon");
+
+//javascript는 html에 접근할 수있다. (html에서 <script src="./practice.js"></script>  을 해줬기때문에)
+//그 시작점은 document(=자바스크립트는 html을 document라는 이름의 object 본다.)
+//javascript는 html에 있는 것들을 object 내부의 것들로 바라본다.
+document.getElementById("title");
+
+document.querySelector(".title");
+//이 강의에서는 querySelector을 쓸거다. (element를 CSS selector방식으로 검색할 수 있음)
+//querySelector는 첫번째것만 가져오고, 전체다 가져오고 싶으면 querySelectorAll을 쓰면됨
+
+//javascript가 event 에 접근하는 방법. (방법1))addEventListener 을 써준다. ->이벤트이름/실행할 function)
+const title = document.querySelector("h1");
+function clicked() {
+  console.log("it is clicked!");
+}
+//방법1))
+title.addEventListener("click", clicked); //function은 ()을 쓰지X, 일단은 "click"되기전까지 실행하면 안되니까 ()를 쓰지 않는다.
+//이 방법은 나중에 addEventListner을 제거할 수 있어서 노마드가 더 선호함
+
+//방법2))
+title.onclick = clicked;
